f17bef0c871d953107f11368ab0f574a3aff3589
amichaiw <Amichai.Wortzman@mail.huji.ac.il>
2025-09-04 11:56:57 +0300

    first commit

---
+++ RootCmp.jsx
@@ -0,0 +1,33 @@
+
+import { AppHeader } from "./cmps/AppHeader.jsx"
+import { Home } from "./cmps/Home.jsx"
+import { HomePage } from "./pages/HomePage.jsx"
+import { AboutUs } from "./pages/AboutUs.jsx"
+import { BookIndex } from "./pages/BookIndex.jsx"
+import { BookDetails } from "./pages/BookDetails.jsx"
+import { BookEdit } from "./pages/BookEdit.jsx"
+
+const Router = ReactRouterDOM.HashRouter
+const { Routes, Route, Navigate } = ReactRouterDOM
+
+
+
+export function RootCmp() {
+    return (
+        <Router>
+            <section className="app">
+                <AppHeader />
+                <main className="main-layout">
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/about" element={<AboutUs />} />
+                        <Route path="/book" element={<BookIndex />} />
+                        <Route path="/book/:bookId " element={<BookDetails />} />
+                        <Route path="/book/edit " element={<BookEdit />} />
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                </main>
+            </section>
+        </Router>
+    )
+}
