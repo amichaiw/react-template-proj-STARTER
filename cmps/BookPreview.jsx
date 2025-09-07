e1da7a96907d20ded23c76162ba63640f34b42af
amichaiw <Amichai.Wortzman@mail.huji.ac.il>
2025-09-07 12:47:02 +0300

    refactor BookPreview component structure; update CSS styles and main title

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,12 +1,12 @@
 
-
-
 export function BookPreview({book}) {
     return (
         <article className="book-preview">
             <h2>{book.title}</h2>
-            <h4>{book.subtitle}</h4>
+            <h3>{book.authors}</h3>
+            {/* <h4>{book.subtitle}</h4> */}
             <img src={book.thumbnail} alt={book.title} />
+            <h4>{book.listPrice.amount} {book.listPrice.currencyCode}</h4>
         </article>
     )
 }
